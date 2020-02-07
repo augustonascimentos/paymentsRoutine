980e03092f6cc641dbc39fad406a3e2e07202c67
augustonascimentos <augustonascimentos@hotmail.com>
2020-02-06 22:39:25 -0300

    preparing setup.py

---
+++ setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+from __future__ import unicode_literals
+import codecs
+import os
+import re
+from setuptools import setup, find_packages
+
+
+def read(*parts):
+    with codecs.open(os.path.join(*parts), 'r') as fp:
+        return fp.read()
+
+
+def get_version():
+    version_file = read('src', '__init__.py')
+    version_match = re.search(r'^__version__ = [\'"]([^\'"]*)[\'"]', version_file, re.MULTILINE)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError('Unable to find version string.')
+
+
+install_requires = [
+    'flask==1.1.1',
+    'flask-cors==3.0.8',
+    'requests==2.22.0',
+    'boto3==1.10.27',
+]
+
+
+setup(
+    name='Payments Routine',
+    version=get_version(),
+    description='This application is part of a payment processing platform',
+    long_description=read('README.md'),
+    long_description_content_type='text/markdown',
+    author='Augusto Nascimento',
+    author_email='augustonascimentos@gmail.com',
+    url='https://github.com/augustonascimentos/paymentsRoutine',
+    packages=find_packages(exclude=("tests", "tests.*")),
+    install_requires=install_requires,
+    include_package_data=True,
+    test_suite="tests",
+    classifiers=[
+    ],
+)
