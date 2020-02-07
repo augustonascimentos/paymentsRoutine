41bd8b9bc347b912b886fa2d182377ebdaf59a1b
augustonascimentos <augustonascimentos@hotmail.com>
2020-02-07 00:03:07 -0300

    test develop

---
+++ src/main/flask_routes/main.py
@@ -0,0 +1,15 @@
+import logging
+from flask import jsonify
+from src.main.resources.ErrorResponse import ErrorResponse
+
+
+def Main(app):
+    @app.route('/v1/accounts/<string:account_id>', methods=['PATCH'])
+    def account(account_id):
+        try:
+            return jsonify(), 200
+
+        except Exception as e:
+            logging.warning('Error: {}'.format(str(e)))
+            response = ErrorResponse().create(e, category='payments')
+            return jsonify(response), response['error']['status_code']
